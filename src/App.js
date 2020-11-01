519cc4be22ea11f90e54c03635691467549a7e19
Saikrishna Reddy <krishnasvr9393@gmail.com>
2020-11-01 18:32:55 +0530

    Initial Commit : Task finnovatioz

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { Line } from "react-chartjs-2";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    let getCoins = async () => {
+      try {
+        var data;
+        await fetch("http://localhost:4000/")
+          .then((res) => {
+            return res.json();
+          })
+          .then((response) => {
+            console.log("res: " + JSON.stringify(response.data));
+            setCoins(response.data)
+          });
+      }
+      catch (error) {
+        console.log(error)
+       }
+    };
+    getCoins();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="coin-app">
+      <h1>CryptoCurrency Tracker App</h1>
+      <h3>Using : ReactJS + CharJs + Nodejs + ExpressJS & Coingecko API</h3>
+      <Line
+        data={{
+          labels: coins.map((coin) => coin.name),
+          datasets: [
+            {
+              data: coins.map((coin) => coin.price_change_percentage_24h),
+              label: "price change",
+              borderColor: "blue",
+              backgroundColor: coins.map((coin) =>
+                coin.price_change_percentage_24h >= 0 ? "green" : "red"
+              ),
+              borderWidth: 5,
+              fill: false,
+              pointHoverRadius: 15,
+            },
+            {
+              data: coins.map((coin) => coin.market_cap_change_percentage_24h),
+              label: "market cap change",
+              borderColor: "green",
+              backgroundColor: coins.map((coin) =>
+                coin.market_cap_change_percentage_24h >= 0 ? "green" : "red"
+              ),
+              borderWidth: 5,
+              fill: false,
+              pointHoverRadius: 15,
+            },
+          ],
+        }}
+        options={{
+          responsive: true,
+          maintainAspectRatio: true,
+        }}
+      />
+      <Line
+        data={{
+          labels: coins.map((coin) => coin.name),
+          datasets: [
+            {
+              data: coins.map((coin) => coin.ath_change_percentage),
+              label: "All time high percentage Change",
+              borderColor: "#008cff",
+              backgroundColor: "white",
+              borderWidth: 5,
+              fill: false,
+              pointHoverRadius: 15,
+            },
+          ],
+        }}
+        options={{
+          responsive: true,
+          maintainAspectRatio: true,
+        }}
+      />
     </div>
   );
 }
---
+++ src/backend/api.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const CoinGecko = require("coingecko-api");
+const app = express();
+var cors = require("cors");
+
+app.use(cors())
+const port = 4000;
+
+const CoinGeckoClient = new CoinGecko();
+
+app.get("/", (req, res) => {
+  var getCoins = async () => {
+    try {
+      var data = await CoinGeckoClient.coins.markets({
+        per_page: 10,
+      });
+        // console.log(data.data)
+    } catch (error) {
+      console.log("Failed to fetch data back");
+    }
+      res.send(data);
+  };
+  getCoins();
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
